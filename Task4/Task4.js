baf7f21c86c8d52303e519bd86dcb6ae0ddde4a7
YaroKl <wolfenstein169@gmail.com>
2022-03-30 21:37:01 +0300

    Завершенные задания 1-4

--- Task4/Task4.js
+++ Task4/Task4.js
@@ -1,4 +1,8 @@
 var colorArray = ["#5A9C6E", "#A8BF5A", "#FAC46E", "#FAD5BB", "#F2FEFF"];
+DarkTheme = "#1E1E1E"
+BrightTheme = "#FFF0F0"
+DarkThemeFont = "#FFFFFF"
+BrightThemeFont = "#000000"
 var i = 0;
 
 function changeColor(){
@@ -7,4 +11,14 @@ function changeColor(){
     if(i > colorArray.length - 1){
         i = 0;
     }
+}
+
+function changeDarkTheme(){
+    document.body.style.background = DarkTheme;
+    document.body.style.color = DarkThemeFont;
+}
+
+function changeBrightTheme(){
+    document.body.style.background = BrightTheme;
+    document.body.style.color = BrightThemeFont;
 }
