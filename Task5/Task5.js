596a93dab64993723c2b431deda508a4304c28ac
YaroKl <wolfenstein169@gmail.com>
2022-04-06 16:42:00 +0300

    Задания 5-8

--- Task5/Task5.js
+++ Task5/Task5.js
@@ -2,13 +2,13 @@ const circles = document.querySelectorAll(".circle");
 console.log(circles);
 
 let active = 0;
-setInterval("change();", 3000);
+setInterval("change();", 1000);
 
 function change()
 {
     circles[active].className = 'circle';
     active++;
-    if (active > 2)
+    if (active > 3)
     {
         active = 0;
     }
